a56227f45eb08fb12be84d361ca5b12af77273ef
azishio <azishio3141@gmail.com>
2024-03-23 03:28:50 +0900

    ✨➕ 環境変数で指定したパスの画像をコメント付きで投稿

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import {TwitterApi} from "twitter-api-v2";
+
+const client = new TwitterApi({
+    appKey:process.env.X_API_KEY,
+    appSecret:process.env.X_API_SECRET,
+    accessToken:process.env.X_ACCESS_TOKEN,
+    accessSecret:process.env.X_ACCESS_SECRET,
+
+})
+
+const mediaId = await client.v1.uploadMedia(process.env.IMAGE_PATH as string);
+
+const postMsg = `実験中
+「青岩公園」
+${Date()}`;
+
+client.v2.tweet("hello world",{media:{media_ids:[mediaId]}});
