ff7c38d5b2505e8f717b6357c70ec1b7628869d7
azishio <azishio3141@gmail.com>
2024-03-25 01:26:11 +0900

    ➕⚡️ 投稿する画像の輝度を正規化
    暗い画像でも見やすくなった

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import {$} from "bun";
+import sharp from "sharp";
 import {EUploadMimeType, TwitterApi} from "twitter-api-v2";
 import {bundled_env} from "./bundled_env.ts" with {type: "macro"};
 
@@ -6,17 +7,14 @@ const client = new TwitterApi(bundled_env().x_token);
 
 const { height, width } = bundled_env().camera;
 
-const image = Buffer.from(
-	await (
-		await $`libcamera-jpeg -o - -t 1 -n --width ${width} --height ${height}`
-			.env({
-				PATH: "/usr/bin", // `libcamera-jpeg`の場所},
-			})
-			.blob()
-	).arrayBuffer(),
-);
+const image =
+	await $`libcamera-jpeg -o - -t 1 -n --width ${width} --height ${height}`
+		.env({ PATH: "/usr/bin" }) // `libcamera-jpeg`の場所
+		.arrayBuffer();
 
-const mediaId = await client.v1.uploadMedia(image, {
+const normalizedImage = await sharp(image).normalize({ upper: 90 }).toBuffer();
+
+const mediaId = await client.v1.uploadMedia(normalizedImage, {
 	mimeType: EUploadMimeType.Jpeg,
 });
 
