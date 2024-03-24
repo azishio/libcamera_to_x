65df5ff6e68d16afaefac2bc64f63ccebedbd5eb
azishio <azishio3141@gmail.com>
2024-03-24 12:01:21 +0900

    🧑‍💻 Biomeの導入
    フォーマッターとしてBiomeを導入。
    以降のコミットはBiomeのFormatとSafe fixesをかけた状態で行なう。

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
-import {TwitterApi} from "twitter-api-v2";
-import {$} from "bun"
 import * as path from "path";
+import { $ } from "bun";
+import { TwitterApi, type TwitterApiTokens } from "twitter-api-v2";
 
 const client = new TwitterApi({
-    appKey:process.env.X_API_KEY,
-    appSecret:process.env.X_API_SECRET,
-    accessToken:process.env.X_ACCESS_TOKEN,
-    accessSecret:process.env.X_ACCESS_SECRET,
-
-})
+	appKey: process.env.X_API_KEY,
+	appSecret: process.env.X_API_SECRET,
+	accessToken: process.env.X_ACCESS_TOKEN,
+	accessSecret: process.env.X_ACCESS_SECRET,
+});
 
 console.log(path.dirname(Bun.main));
 
-const imageFile = `${path.dirname(Bun.main)}/shimonita.jpg`
+const imageFile = `${path.dirname(Bun.main)}/shimonita.jpg`;
 
-await $`libcamera-jpeg -o ${imageFile} -t 1 --width ${Bun.env.WIDTH} --height ${Bun.env.HEIGHT}`
+await $`libcamera-jpeg -o ${imageFile} -t 1 --width ${Bun.env.WIDTH} --height ${Bun.env.HEIGHT}`;
 
 const mediaId = await client.v1.uploadMedia(imageFile);
 
@@ -22,4 +21,4 @@ const postMsg = `実験中
 「青岩公園」
 ${Date()}`;
 
-client.v2.tweet(postMsg,{media:{media_ids:[mediaId]}});
+client.v2.tweet(postMsg, { media: { media_ids: [mediaId] } });
