e9432620deb09fccba41fefa8ad8888ebd1df7a9
azishio <azishio3141@gmail.com>
2024-03-24 13:37:05 +0900

    💬 プロジェクトをより汎用的に
    + 下仁田青岩公園に関係するリテラルの変更
    + プロジェクト名を`libcamera_to_x`へ変更

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const client = new TwitterApi(bundled_env().x_token);
 console.log(path.dirname(Bun.main));
 
 // TODO 一度ファイルに出力せず、画像バイナリを直接受け取れるようにする方法を探す
-const imageFilePath = "~/shimonita_x_picture.jpg";
+const imageFilePath = "~/libcamera_captured_picture.jpg";
 
 const { width, height } = bundled_env().camera;
 
@@ -20,8 +20,4 @@ await $`libcamera-jpeg -o ${imageFilePath} -t 1 --width ${width} --height ${heig
 
 const mediaId = await client.v1.uploadMedia(imageFilePath);
 
-const postMsg = `実験中
-「青岩公園」
-${Date()}`;
-
-client.v2.tweet(postMsg, { media: { media_ids: [mediaId] } });
+client.v2.tweet(Date(), { media: { media_ids: [mediaId] } });
